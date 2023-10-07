01dae69795a133e64c8f3c88bfe10648c2129f85
f-fagua <felipe.fagua@unity3d.com>
2023-10-07 11:54:45 -0500

    first version of the checker (testing with local bundles, none chached)

--- Assets/Loader.cs
+++ Assets/Loader.cs
@@ -1,7 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.AddressableAssets.ResourceLocators;
+using UnityEngine.ResourceManagement.ResourceLocations;
+using UnityEngine.ResourceManagement.ResourceProviders;
 
 public class Loader : MonoBehaviour
 {
@@ -21,26 +25,56 @@ public class Loader : MonoBehaviour
 
             Invoke("LoadAsset", 5);
         };
-
-        
-    }
-
-    private void GetCachedVersions()
-    {
-        foreach (var resourceLocator in Addressables.ResourceLocators)
-        {
-            foreach (var key in resourceLocator.Keys)
-            {
-                Debug.Log(key);
-            }
-        }
     }
 
     private void LoadAsset()
     {
         m_Asset.InstantiateAsync().Completed += handle =>
         {
-            GetCachedVersions();    
+            var bundles = GetBundlesFromCatalog();
+            ValidateCachedBundle(bundles);
         };
+    }
+
+    private List<CachedAssetBundle> GetBundlesFromCatalog()
+    {
+        List<CachedAssetBundle> catalogBundles = new List<CachedAssetBundle>();
+        foreach (var resourceLocator in Addressables.ResourceLocators)
+        {
+            if (resourceLocator is not ResourceLocationMap) continue;
+            var catalog = (ResourceLocationMap) resourceLocator;
+            
+            foreach (var location in catalog.Locations)
+                if(LocationToCachedBundleData(location, out var cachedAssetBundle))
+                    catalogBundles.Add(cachedAssetBundle);
+        }
+
+        return catalogBundles;
+    }
+
+    private bool LocationToCachedBundleData(KeyValuePair<object, IList<IResourceLocation>> location, out CachedAssetBundle cachedAssetBundle)
+    {
+        cachedAssetBundle = new CachedAssetBundle();
+        if (location.Key.ToString().EndsWith(".bundle"))
+        {
+            if (location.Value[0].Data is not AssetBundleRequestOptions) return false;
+                    
+            var loadData = (AssetBundleRequestOptions) location.Value[0].Data;
+            cachedAssetBundle.name = loadData.BundleName;
+            cachedAssetBundle.hash = Hash128.Parse(loadData.Hash);
+            return true;
+        }
+
+        return false;
+    }
+    
+    private void ValidateCachedBundle(List<CachedAssetBundle> bundles)
+    {
+        var msj = new StringBuilder();
+        foreach (var bundle in bundles)
+        {
+            msj.AppendLine($"name: {bundle.name}, hash: {bundle.hash}, cached: {Caching.IsVersionCached(bundle)}");
+        }
+        Debug.Log(msj);
     }
 }
